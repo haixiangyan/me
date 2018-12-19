cbbed5b767341da84ffa2b67c47ce061201c2668
Haixiang <haixiang6123@gmail.com>
2018-12-19 13:41:07 -0800

    Update: use react-redux

--- codes/react-redux/src/App.js
+++ codes/react-redux/src/App.js
@@ -1,34 +1,34 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 class App extends Component {
-    add1() {
-        this.props.store.dispatch({
-            type: 'add',
-            payload: 1
-        })
-    }
-    add2() {
-        this.props.onAdd2()
-    }
-    add3() {
-        this.props.onAdd3()
-    }
-    add4() {
-        this.props.onAdd4()
-    }
     render() {
         return (
             <div className="App">
-                Clicked: <span id="value">{this.props.value}</span> times
+                Clicked: <span id="value">{this.props.n}</span> times
                 <div>
-                    <button id="add1" onClick={this.add1.bind(this)}>+1</button>
-                    <button id="add2" onClick={this.add2.bind(this)}>+2</button>
-                    <button id="addIfOdd" onClick={this.add3.bind(this)} >Increment if odd</button>
-                    <button id="addAsync" onClick={this.add4.bind(this)}>Increment async</button>
+                    <button id="add1" onClick={this.props.add1.bind(this)}>+1</button>
+                    <button id="add2" >+2</button>
+                    <button id="addIfOdd" >Increment if odd</button>
+                    <button id="addAsync" >Increment async</button>
                 </div>
             </div>
         )
     }
 }
 
-export default App
+// 将部分 store 里的 state 映射到 props 上
+function mapStateToProps(state) {
+    return {
+        n: state.n
+    }
+}
+
+// 生成 action 相关操作
+const mapDispatchToProps =  {
+    add1: () => {
+        return { type: 'add', payload: 1 }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- codes/react-redux/src/index.js
+++ codes/react-redux/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const stateChanger = (state, action) => {
     if (state === undefined) {
-        return 0
+        return { n: 0 }
     }
     else {
         if (action.type === 'add') {
-            let newState = state + action.payload
+            let newState =  {
+                n: state.n + action.payload
+            }
             return newState
         }
         else {
@@ -21,41 +24,13 @@ const stateChanger = (state, action) => {
 }
 
 const store = createStore(stateChanger)
-store.subscribe(render)
 
-function add3() {
-    if (store.getState() % 2 === 1) {
-        store.dispatch({
-            type: 'add',
-            payload: 1
-        })
-    }
-}
-
-function add4() {
-    setTimeout(() => {
-        store.dispatch({
-            type: 'add',
-            payload: 1
-        })
-    }, 2000)
-}
-
-function render() {
-    ReactDOM.render(
-        <App
-            value={store.getState()}
-            store={store}
-            onAdd1={() => {store.dispatch({type: 'add', payload: 1})}}
-            onAdd2={() => {store.dispatch({type: 'add', payload: 2})}}
-            onAdd3={add3}
-            onAdd4={add4}
-        />,
-        document.getElementById('root')
-    )
-}
-
-render()
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
