d14bcce2970d98116d0802e03da5ae03367ef7e2
Haixiang <haixiang6123@gmail.com>
2019-03-07 12:16:14 -0800

    Finish profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="profile">
-        Profile
+        <h1 class="profile-h1">Hey, I'm Haixiang</h1>
+        <h2 class="profile-h2">A fun guy and a front-end engineer</h2>
+        <img class="profile-avatar" src="../assets/avatar.jpg" alt="avatar">
     </div>
 </template>
 
@@ -12,7 +14,27 @@
 
 <style scoped lang="scss">
 .profile {
-    width: 50vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     border-radius: 10px;
+    height: 85vh;
+    &-h1 {
+        font-size: 5em;
+        font-weight: normal;
+        letter-spacing: .1em;
+    }
+    &-h2 {
+        margin: 20px 0;
+        font-size: 2em;
+        font-weight: normal;
+        letter-spacing: .1em;
+    }
+
+    &-avatar {
+        width: 30%;
+        border-radius: 50%;
+    }
 }
 </style>
