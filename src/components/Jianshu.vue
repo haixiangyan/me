2143602192029c21cfa686a941575289cad0d1b9
Haixiang6123 <haixiang6123@gmail.com>
2019-03-07 09:38:22 -0800

    Redo blog project

--- src/components/Jianshu.vue
+++ src/components/Jianshu.vue
@@ -1,16 +1,5 @@
 <template>
     <div class="jianshu">
-        <div class="jianshu-left">
-            
-        </div>
-        <div class="jianshu-right">
-            <div>
-                <img src="../assets/jianshu.jpg" alt="jianshu banner">
-            </div>
-            <div>
-                <img src="../assets/jianshu.jpg" alt="jianshu banner">
-            </div>
-        </div>
     </div>
 </template>
 
@@ -28,34 +17,6 @@
 
 <style scoped lang="scss">
 .jianshu {
-    position: fixed;
-    bottom: -15vh;
-    left: 2vw;
-    height: 110vh;
-    width: 45vw;
-    display: flex;
-    border-radius: 10px;
-    box-shadow: 0px 0px 24px 0px #EA7058;
-    transform: rotate(-30deg);
-    transform-origin: left bottom;
-    transition: all .5s;
-    overflow: auto;
-    background: #EA7058;
-    &:hover {
-        z-index: 3;
-        transform: scale(1.2) rotate(-5deg);
-    }
-    &-left {
-        width: 10%;
-        height: 110vh;
-    }
-    &-right {
-        width: 90%;
-        img {
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
-            width: 100%;
-        }
-    }
+
 }
 </style>
--- src/components/Medium.vue
+++ src/components/Medium.vue
@@ -1,16 +1,5 @@
 <template>
     <div class="medium">
-        <div class="medium-left">
-            <div>
-                <img src="../assets/medium.jpg" alt="medium banner">
-            </div>
-            <div>
-                <img src="../assets/medium.jpg" alt="medium banner">
-            </div>
-        </div>
-        <div class="medium-right">
-
-        </div>
     </div>
 </template>
 
@@ -22,33 +11,5 @@
 
 <style scoped lang="scss">
 .medium {
-    position: fixed;
-    bottom: -10vh;
-    right: 2vw;
-    height: 110vh;
-    width: 40vw;
-    display: flex;
-    border-radius: 10px;
-    box-shadow: 0px 0px 24px 0px #66C3A0;
-    transform: rotate(30deg);
-    transform-origin: right bottom;
-    transition: all .5s;
-    overflow: auto;
-    background: #66C3A0;
-    &:hover {
-        z-index: 3;
-        transform: scale(1.2) rotate(5deg);
-    }
-    &-right {
-        width: 10%;
-    }
-    &-left {
-        width: 90%;
-        img {
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
-            width: 100%;
-        }
-    }
 }
 </style>
