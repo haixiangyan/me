e7ef8b836007f1429d193f2684f5b8f0100c6325
Haixiang <haixiang6123@gmail.com>
2019-03-07 12:27:43 -0800

    Remove importing iview globally

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
-
-Vue.use(iView)
+import './plugins/index'
 
 Vue.config.productionTip = false
 
---
+++ src/plugins/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import 'iview/dist/styles/iview.css';
+import { Menu, MenuItem, Icon } from 'iview';
+
+Vue.component('Menu', Menu);
+Vue.component('MenuItem', MenuItem);
+Vue.component('Icon', Icon);
