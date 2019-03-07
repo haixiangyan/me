fe4472f71533b70d76690274691fb5311adc1df8
Haixiang <haixiang6123@gmail.com>
2019-03-07 15:59:41 -0800

    Remove date property

--- src/components/Jianshu/JianshuItem.vue
+++ src/components/Jianshu/JianshuItem.vue
@@ -4,7 +4,6 @@
             <div class="jianshu-item-left">
                 <a class="jianshu-item-title">{{item.title}}</a>
                 <p class="jianshu-item-desc">{{item.desc}}</p>
-                <p class="jianshu-item-date">{{item.date}}</p>
             </div>
             <div class="jianshu-item-right">
                 <img class="jianshu-item-img" :src="item.avatar" alt="">
