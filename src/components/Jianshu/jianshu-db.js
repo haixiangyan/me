394ac3ad52c407fe05392c53b91b0723017fcce7
Haixiang <haixiang6123@gmail.com>
2019-03-07 15:51:10 -0800

    Add db files

---
+++ src/components/Jianshu/jianshu-db.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+    {
+        title: 'TS: 深入浅出',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        date: new Date().getTime()
+    },
+]
---
+++ src/components/Medium/medium-db.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+    {
+        title: 'Vue: Two-Way Binding',
+        avatar: 'https://miro.medium.com/fit/c/1400/420/1*Ht8T-vqbqy5iG7FzNQGjFA.png',
+        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        date: new Date().getTime()
+    },
+]
