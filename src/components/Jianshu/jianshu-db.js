e5ed57cae093fc4daa937da331d0fa95990fc97e
Haixiang <haixiang6123@gmail.com>
2019-03-07 17:57:44 -0800

    Update typo

--- src/components/Jianshu/jianshu-db.js
+++ src/components/Jianshu/jianshu-db.js
@@ -1,8 +1,8 @@
 export default [
     {
         title: 'TS: 深入浅出',
-        desc: 'Two-way binding is so popular in Vue.js. Many developers love to use v-model directive in their Web APP. Talking about how…',
+        desc: '接口算是官方文档里的第一个知识点了，这篇文章会简单介绍 TypeScrip 里的接口。 类型 在说接口之前我们先来了解下 TypeScript ...',
         avatar: 'https://upload-images.jianshu.io/upload_images/2979799-40a3d166d2178e96.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
-        url: '/'
+        url: 'https://www.jianshu.com/p/6d6b6972182f'
     },
 ]
