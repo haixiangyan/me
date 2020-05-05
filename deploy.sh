acf4ba71faba9d7e814ce9c2eabc6d4c5772116c
Haixiang <haixiang6123@gmail.com>
2020-05-04 19:13:09 -0700

    更新部署配置

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,13 @@ npm run build
 cd dist
 
 # 部署到自定义域域名
-# echo 'www.example.com' > CNAME
+ echo 'yanhaixiang.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:Haixiang6123/Haixiang6123.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Haixiang6123/blog.git master:gh-pages
+ git push -f git@github.com:Haixiang6123/Haixiang6123.github.io.git master
 
 cd -
