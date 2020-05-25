ee697ffc82cc5a26e3498a8506c63f4e8f352a7a
Haixiang <haixiang6123@gmail.com>
2020-05-25 20:37:37 +0800

    清理没有用到的 element ui 组件

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,34 +1,18 @@
 import Vue from 'vue';
 import {
-  Menu,
-  MenuItem,
-  Submenu,
   Icon,
-  Divider,
-  Row,
-  Col,
-  Tooltip,
   Pagination,
   Carousel,
   CarouselItem,
   Button,
-  Radio,
-  Link
+  Radio
 } from 'element-ui'
 
 import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.component(Menu.name, Menu);
-Vue.component(MenuItem.name, MenuItem);
-Vue.component(Submenu.name, Submenu);
 Vue.component(Icon.name, Icon);
-Vue.component(Divider.name, Divider);
-Vue.component(Row.name, Row);
-Vue.component(Col.name, Col);
-Vue.component(Tooltip.name, Tooltip);
 Vue.component(Pagination.name, Pagination);
 Vue.component(Carousel.name, Carousel);
 Vue.component(CarouselItem.name, CarouselItem);
 Vue.component(Button.name, Button);
 Vue.component(Radio.name, Radio);
-Vue.component(Link.name, Link);
