14ec40a993d43f100b4a8949f6ad96375c99a2cc
Haixiang <haixiang6123@gmail.com>
2020-05-25 15:10:15 +0800

    拆分 Social 组件

--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -3,104 +3,57 @@
         <img class="profile-avatar" :src="avatar" alt="avatar">
         <h1 class="profile-title">哈啰！我是海翔</h1>
         <h2 class="profile-subtitle">年轻人不要熬夜 :)</h2>
-        <div class="profile-social">
-            <span class="profile-social-item">
-                <a href="https://github.com/Haixiang6123" target="_blank">
-                    <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-github"></use>
-                    </svg>
-                </a>
-            </span>
-            <span class="profile-social-item">
-                <a href="mailto:haixiang6123@gmail.com" target="_blank">
-                    <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-mail"></use>
-                    </svg>
-                </a>
-            </span>
-            <span class="profile-social-item">
-                <a href="https://www.jianshu.com/u/0340be4082b5" target="_blank">
-                    <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-jianshu"></use>
-                    </svg>
-                </a>
-            </span>
-            <span class="profile-social-item">
-                <a href="https://medium.com/@haixiang6123" target="_blank">
-                    <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-medium"></use>
-                    </svg>
-                </a>
-            </span>
-            <span class="profile-social-item">
-                <a href="https://playoverwatch.com/en-us/career/pc/Monster-14255" target="_blank">
-                    <svg class="icon" aria-hidden="true">
-                        <use xlink:href="#icon-overwatch"></use>
-                    </svg>
-                </a>
-            </span>
-        </div>
+        <Social/>
     </div>
 </template>
 
 <script>
-    import {getImageUrl} from "../BucketList/list"
+  import {getImageUrl} from "../BucketList/list"
+  import Social from "./Social"
 
-    export default {
-        name: "Profile",
-        data() {
-          return {
-            avatar: getImageUrl('avatar.jpg')
-          }
-        }
+  export default {
+    name: "Profile",
+    data() {
+      return {
+        avatar: getImageUrl('avatar.jpg')
+      }
+    },
+    components: {
+      Social
     }
+  }
 </script>
 
 <style scoped lang="scss">
-.profile {
-    margin-top: -70px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border-radius: 10px;
-    height: 85vh;
-
-    &-avatar {
-        margin: 20px 0;
-        width: 200px;
-        border-radius: 50%;
-    }
-
-    &-title {
-        font-size: 3.4em;
-        font-weight: normal;
-        letter-spacing: .1em;
-        text-align: center;
-    }
-    &-subtitle {
-        margin: 20px 0;
-        font-size: 1.3em;
-        font-weight: normal;
-        letter-spacing: .1em;
-        text-align: center;
-    }
-
-    &-social {
-        margin-top: 12px;
-        width: 100%;
+    .profile {
+        margin-top: -70px;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
-        &-item {
-            & + & {
-                margin-left: 40px;
-            }
+        border-radius: 10px;
+        height: 85vh;
+
+        &-avatar {
+            margin: 20px 0;
+            width: 200px;
+            border-radius: 50%;
         }
-        svg {
-            height: 20px;
-            width: 20px;
+
+        &-title {
+            font-size: 3.4em;
+            font-weight: normal;
+            letter-spacing: .1em;
+            text-align: center;
         }
+
+        &-subtitle {
+            margin: 20px 0;
+            font-size: 1.3em;
+            font-weight: normal;
+            letter-spacing: .1em;
+            text-align: center;
+        }
+
     }
-}
 </style>
---
+++ src/components/Profile/Social.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="social">
+        <span class="social-item">
+            <a href="https://github.com/Haixiang6123" target="_blank">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-github"></use>
+                </svg>
+            </a>
+        </span>
+        <span class="social-item">
+            <a href="mailto:haixiang6123@gmail.com" target="_blank">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-mail"></use>
+                </svg>
+            </a>
+        </span>
+        <span class="social-item">
+            <a href="https://www.jianshu.com/u/0340be4082b5" target="_blank">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-jianshu"></use>
+                </svg>
+            </a>
+        </span>
+        <span class="social-item">
+            <a href="https://medium.com/@haixiang6123" target="_blank">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-medium"></use>
+                </svg>
+            </a>
+        </span>
+        <span class="social-item">
+            <a href="https://playoverwatch.com/en-us/career/pc/Monster-14255" target="_blank">
+                <svg class="icon" aria-hidden="true">
+                    <use xlink:href="#icon-overwatch"></use>
+                </svg>
+            </a>
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {}
+</script>
+
+<style scoped lang="scss">
+    .social {
+        margin-top: 12px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        &-item {
+            & + & {
+                margin-left: 40px;
+            }
+        }
+
+        svg {
+            height: 20px;
+            width: 20px;
+        }
+    }
+</style>
