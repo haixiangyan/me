508d7b57516d08be5f30ece5f93360941076e12f
Haixiang <haixiang6123@gmail.com>
2020-06-09 17:37:16 +0800

    添加码云图床

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
-export const imgBaseUrl = 'https://raw.githubusercontent.com/Haixiang6123/pic-bed/master/blog'
+export const githubImgBaseUrl = 'https://raw.githubusercontent.com/Haixiang6123/pic-bed/master/blog'
+export const giteeImgBaseUrl = 'https://gitee.com/Haixiang6123/pic-bed/raw/master/blog'
 
-export const getImageUrl = (fileName: string) => `${imgBaseUrl}/${fileName}`
+export const getImageUrl = (fileName: string) => `${giteeImgBaseUrl}/${fileName}`
