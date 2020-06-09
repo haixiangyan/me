aefb08df946a8b203e727e2fe2b67342f84803c9
Haixiang <haixiang6123@gmail.com>
2020-06-10 00:31:17 +0800

    添加部署到 coding.net 文件

---
+++ coding-deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下
+cd dist
+
+# 部署到自定义域域名
+echo 'yanhaixiang.cn' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+git push -f git@e.coding.net:hai_guai/hai_guai.coding.io.git master
+
+cd -
