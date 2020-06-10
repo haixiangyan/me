3f63f63f8625ef827aaded4e1550c94df512a6f3
Haixiang <haixiang6123@gmail.com>
2020-06-10 14:25:27 +0800

    自动切换图片地址

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,7 @@
 export const githubImgBaseUrl = 'https://raw.githubusercontent.com/Haixiang6123/pic-bed/master/blog'
 export const giteeImgBaseUrl = 'https://gitee.com/Haixiang6123/pic-bed/raw/master/blog'
 
-export const getImageUrl = (fileName: string) => `${giteeImgBaseUrl}/${fileName}`
+export const getImageUrl = (fileName: string) =>
+  `${isChina() ? giteeImgBaseUrl : githubImgBaseUrl}/${fileName}`
+
+export const isChina = () => window.location.href === 'https://www.yanhaixiang.cn'
