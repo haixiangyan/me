404d00e78eae883dcaa265d832e2733e0be32ab0
Haixiang <haixiang6123@gmail.com>
2020-06-10 15:18:53 +0800

    自动切换项目 base url

--- src/db/portfolioDB.ts
+++ src/db/portfolioDB.ts
@@ -1,51 +1,53 @@
+import {getRepoUrl} from '@/lib/constants'
+
 const portfolioDB: TPortfolioItem[] = [
   {
     title: '沙雕的《爱与诚》',
     desc: '一个测试你应该做猫还是狗还是做情人的沙雕网站。使用原生 JavaScript 实现。',
-    url: 'https://yanhaixiang.com/cat-dog-lover/',
+    url: getRepoUrl('cat-dog-lover'),
     github: 'https://github.com/Haixiang6123/cat-dog-lover',
     languages: ['JavaScript'],
     stars: 0
   },
   {
-    'title': '仿微信记账本',
-    'desc': '一个基于 React.ts 开发的仿微信记账本，全程使用函数组件和 React Hooks。',
-    'url': 'https://haixiang6123.gitee.io/weixin-cash/',
-    'github': 'https://github.com/Haixiang6123/weixin-cash',
-    'languages': ['React', 'TypeScript'],
+    title: '仿微信记账本',
+    desc: '一个基于 React.ts 开发的仿微信记账本，全程使用函数组件和 React Hooks。',
+    url: getRepoUrl('weixin-cash'),
+    github: 'https://github.com/Haixiang6123/weixin-cash',
+    languages: ['React', 'TypeScript'],
     stars: 0
   },
   {
-    'title': '易推',
-    'desc': '一个基于 Vue.ts + Express.ts 开发的内推系统。包含设计，前后端开发，安全，鉴权，测试，数据库，部署等基本的全栈流程。',
-    'url': 'https://easy-refer.com',
-    'github': 'https://github.com/Haixiang6123/easy-refer',
-    'languages': ['React', 'TypeScript'],
+    title: '易推',
+    desc: '一个基于 Vue.ts + Express.ts 开发的内推系统。包含设计，前后端开发，安全，鉴权，测试，数据库，部署等基本的全栈流程。',
+    url: 'https://easy-refer.com',
+    github: 'https://github.com/Haixiang6123/easy-refer',
+    languages: ['React', 'TypeScript'],
     stars: 0
   },
   {
-    'title': '守望先锋 UI 库',
-    'desc': '一个基于 Vue.js 开发的守望先锋主题 UI 库。纪念当年通宵打守望先锋的大学时光。',
-    'url': 'https://yanhaixiang.com/overwatch-ui-doc/#/',
-    'github': 'https://github.com/Haixiang6123/overwatch-ui',
-    'languages': ['Vue'],
-    'stars': 344
+    title: '守望先锋 UI 库',
+    desc: '一个基于 Vue.js 开发的守望先锋主题 UI 库。纪念当年通宵打守望先锋的大学时光。',
+    url: getRepoUrl('overwatch-ui-doc'),
+    github: 'https://github.com/Haixiang6123/overwatch-ui',
+    languages: ['Vue'],
+    stars: 344
   },
   {
-    'title': '代码美化 Chrome 插件',
-    'desc': '一个可以一键美化网页中的代码块的 Chrome 插件。',
-    'url': 'https://chrome.google.com/webstore/detail/code-block-beautifier/gpcjjddhdnilcbddlonlfgdbejfboonn',
-    'github': 'https://github.com/Haixiang6123/codeblock-beautifier',
-    'languages': ['Chrome'],
-    'stars': 101
+    title: '代码美化 Chrome 插件',
+    desc: '一个可以一键美化网页中的代码块的 Chrome 插件。',
+    url: 'https://chrome.google.com/webstore/detail/code-block-beautifier/gpcjjddhdnilcbddlonlfgdbejfboonn',
+    github: 'https://github.com/Haixiang6123/codeblock-beautifier',
+    languages: ['Chrome'],
+    stars: 101
   },
   {
-    'title': '在线吉他谱编辑器',
-    'desc': '一个基于 React.js 开发的在线吉他谱编辑器。',
-    'url': 'https://yanhaixiang.com/guitar-editor/#/',
-    'github': 'https://github.com/Haixiang6123/guitar-editor',
-    'languages': ['React'],
-    'stars': 47
+    title: '在线吉他谱编辑器',
+    desc: '一个基于 React.js 开发的在线吉他谱编辑器。',
+    url: getRepoUrl('guitar-editor'),
+    github: 'https://github.com/Haixiang6123/guitar-editor',
+    languages: ['React'],
+    stars: 47
   },
   {
     title: 'Leetcode Python版本',
@@ -56,20 +58,20 @@ const portfolioDB: TPortfolioItem[] = [
     stars: 14
   },
   {
-    'title': '猫狗探探',
-    'desc': '一个显示宠物图片的网页应用。分别使用 React.js, Vue.js 和 Angular 实现一遍。',
-    'url': 'https://yanhaixiang.com/awesome-a-cat-a-dog/',
-    'github': 'https://github.com/Haixiang6123/awesome-a-cat-a-dog',
-    'languages': ['Vue', 'React', 'Angular'],
-    'stars': 8
+    title: '猫狗探探',
+    desc: '一个显示宠物图片的网页应用。分别使用 React.js, Vue.js 和 Angular 实现一遍。',
+    url: 'https://yanhaixiang.com/awesome-a-cat-a-dog/',
+    github: 'https://github.com/Haixiang6123/awesome-a-cat-a-dog',
+    languages: ['Vue', 'React', 'Angular'],
+    stars: 8
   },
   {
-    'title': 'wtf-cli',
-    'desc': '一个查询英文缩写的命令行工具。',
-    'url': 'https://www.npmjs.com/package/wtf-cli',
-    'github': 'https://github.com/Haixiang6123/wtf-cli',
-    'languages': ['JavaScript'],
-    'stars': 24
+    title: 'wtf-cli',
+    desc: '一个查询英文缩写的命令行工具。',
+    url: 'https://www.npmjs.com/package/wtf-cli',
+    github: 'https://github.com/Haixiang6123/wtf-cli',
+    languages: ['JavaScript'],
+    stars: 24
   },
 
   {
@@ -97,12 +99,12 @@ const portfolioDB: TPortfolioItem[] = [
     stars: 0
   },
   {
-    'title': '在线画图工具',
-    'desc': '一个在线画图 App。使用原生 JavaScript 来实现画图功能。',
-    'url': 'https://yanhaixiang.com/sketch-book/',
-    'github': 'https://github.com/Haixiang6123/sketch-book',
-    'languages': ['JavaScript'],
-    'stars': 2
+    title: '在线画图工具',
+    desc: '一个在线画图 App。使用原生 JavaScript 来实现画图功能。',
+    url: 'https://haixiang6123.gitee.io/sketch-book/',
+    github: 'https://github.com/Haixiang6123/sketch-book',
+    languages: ['JavaScript'],
+    stars: 2
   }
 ]
 
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,13 @@
 export const githubImgBaseUrl = 'https://raw.githubusercontent.com/Haixiang6123/pic-bed/master/blog'
 export const giteeImgBaseUrl = 'https://gitee.com/Haixiang6123/pic-bed/raw/master/blog'
 
+export const githubResBaseUrl = 'https://haixiang6123.github.io'
+export const giteeResBaseUrl = 'https://haixiang6123.gitee.io'
+
 export const getImageUrl = (fileName: string) =>
   `${isChina() ? giteeImgBaseUrl : githubImgBaseUrl}/${fileName}`
 
-export const isChina = () => window.location.href.includes('.cn')
+export const getRepoUrl = (repo: string) =>
+  `${isChina() ? giteeResBaseUrl : githubResBaseUrl}/${repo}`
+
+export const isChina = () => !window.location.href.includes('.cn')
