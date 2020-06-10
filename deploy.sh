d645d0f371be4b17bd761a2bbc0375682754a8e1
Haixiang <haixiang6123@gmail.com>
2020-06-10 15:26:45 +0800

    添加自定义域名

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,18 @@ yarn build
 # cd 到构建输出的目录下
 cd dist
 
-# 部署到自定义域域名
-# echo 'yanhaixiang.com' > CNAME
-# echo 'yanhaixiang.cn' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
+# 部署到自定义域域名
+echo 'yanhaixiang.com' > CNAME
+
 # 部署到 Github
 git push -f git@github.com:Haixiang6123/Haixiang6123.github.io.git master
+
+# 部署到自定义域域名
+echo 'yanhaixiang.cn' > CNAME
 
 # 部署到 Coding
 git push -f git@e.coding.net:hai_guai/hai_guai.coding.me.git master
