6f7e0bcf054d3bcc0df7a6d50d5faf78c57a4a31
haixiangyan <haixiang6123@gmail.com>
2021-06-24 18:54:13 +0800

    feat: 添加 Project 的 Item 组件

---
+++ src/sections/Project/Item/index.tsx
@@ -0,0 +1,59 @@
+import * as React from "react"
+import {AllHTMLAttributes, FC} from "react"
+import styles from './styles.module.scss'
+import {Tooltip} from "antd"
+
+interface Image {
+  image: string;
+  content: string;
+}
+
+interface Props extends AllHTMLAttributes<HTMLDivElement> {
+  title: string;
+  links: Image[];
+  techUsed: Image[];
+  logo?: string;
+  content?: string;
+  description?: string;
+}
+
+const Item: FC<Props> = (props) => {
+  const {title, links, techUsed, logo, content, description} = props
+
+  return (
+    <li className={styles.item}>
+      {logo && <img src={logo} alt="logo"/>}
+
+      <h4>{title}</h4>
+
+      <div className={styles.content}>{content}</div>
+
+      <div className={styles.description}>{description}</div>
+
+      <ul className={styles.links}>
+        {links.map(link => (
+          <li>
+            <a href={link.content}>
+              <img src={link.image} alt="icon"/>
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles.divider}/>
+
+      <div className={styles.techUsed}>
+        <h4>技术栈</h4>
+        <ul>
+          {techUsed.map(tech => (
+            <li>
+              <img src={tech.image} alt="techUsed"/>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </li>
+  )
+}
+
+export default Item
--- src/sections/Project/index.tsx
+++ src/sections/Project/index.tsx
@@ -2,12 +2,26 @@ import * as React from "react"
 import styles from './styles.module.scss'
 import Title from "../../components/Title"
 import Section from "../../components/Section"
+import Item from "./Item"
 
 const Project = () => {
   return (
     <Section className={styles.project}>
       <Title tag="h2">个人项目</Title>
       <Title tag="h3">超多好玩、沙雕的项目</Title>
+
+      <Item
+        title="This Website!"
+        content="Hello"
+        links={[
+          {image: '', content: 'https://www.baidu.com'}
+        ]}
+        techUsed={[
+          {image: '', content: 'JavaScript'}
+        ]}
+      />
+
+      <Title tag="h3">More projects (and a blog) Coming Soon™ 🕑</Title>
     </Section>
   )
 }
