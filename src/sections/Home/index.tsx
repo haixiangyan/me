f2f157b741954f01fbf8b772e3c4349adb67e102
haixiangyan <haixiang6123@gmail.com>
2021-06-25 02:55:57 +0800

    feat: 实现打字效果

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,25 +1,35 @@
-import {FC} from "react";
+import {FC, useEffect, useRef} from "react";
+import Typed from 'typed.js'
 import styles from './styles.module.scss'
 import handDownSvg from '../../assets/svgs/hand-down.svg'
 
+const strings = [
+  '哈喽，我是帅哥',
+  '等一下，让我想想',
+  `我是海怪，<br>一个热爱生活的奋斗人。<br/>拒绝996和内卷，<br/>偶尔聊聊技术和分享生活。`
+]
+
 const Home: FC = () => {
+  const el = useRef<HTMLParagraphElement | null>(null)
+  const typed = useRef<Typed | null>(null)
+
+  useEffect(() => {
+    const options = {
+      strings,
+      typeSpeed: 100,
+      backSpeed: 60,
+    }
+
+    typed.current = new Typed(el.current || '', options);
+
+    return () => typed.current?.destroy()
+  }, [])
+
   return (
     <section className={styles.home}>
       <div className={styles.content}>
         <div className={styles.intro}>
-          <p>
-            我是海怪，
-          </p>
-          <p>
-            偶尔聊聊技术和分享生活。
-          </p>
-          <p>
-            拒绝996和内卷，
-          </p>
-          <p>
-            一个热爱生活的奋斗人。
-            <span className={styles.cursor}/>
-          </p>
+          <span ref={el}/>
         </div>
 
         <figure className={styles.ballWrapper}>
