20abb0b96ea41ac4045009760c6b97a42c694a8e
haixiangyan <haixiang6123@gmail.com>
2021-06-27 01:39:24 +0800

    feat: 添加链接跳转

--- src/sections/About/index.tsx
+++ src/sections/About/index.tsx
@@ -10,25 +10,26 @@ import HighLight from "../../components/HighLight";
 const About = () => {
   return (
     <Section id="about" className={styles.about}>
-      {/*<Background className={styles.background}/>*/}
+      <Title tag="h2">关于</Title>
+      <Title tag="h3">哈喽，我是海怪 👻<br/>聊聊我自己</Title>
 
-      <div className={styles.content}>
-        <Title tag="h2">关于</Title>
-        <Title tag="h3">哈喽，我是海怪 👻<br/>聊聊我自己</Title>
+      <Profile/>
 
-        <Profile/>
+      <Life/>
 
-        <Life/>
+      <Paragraph className={styles.goal}>
+        对于我来说，<HighLight>前端工程师</HighLight> 更多像是画家，HTML、CSS、JavaScript 就像画笔。
+        <br/>
+        有时候写代码并不是为了做一个 <HighLight>🐂 🍺</HighLight> 的项目，而是把心中的想法实现出来。
+      </Paragraph>
 
-        <Paragraph className={styles.goal}>
-          对于我来说，<HighLight>前端工程师</HighLight> 更多像是画家，HTML、CSS、JavaScript 就像画笔。
-          <br/>
-          有时候写代码并不是为了做一个 <HighLight>🐂 🍺</HighLight> 的项目，而是把心中的想法实现出来。
-        </Paragraph>
-
-        <div>
-          <Button className={styles.pulse} bg="#15a000">Résumé</Button>
-        </div>
+      <div>
+        <Button
+          onClick={() => window.open('https://yanhaixiang.com/resume-generator/', '_blank')}
+          className={styles.pulse}
+          bg="#15a000">
+          Résumé
+        </Button>
       </div>
     </Section>
   )
