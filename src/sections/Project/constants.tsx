f848b8a1d97fe6f04a3250859878ebb6a434ccfe
haixiangyan <haixiang6123@gmail.com>
2021-06-26 12:54:16 +0800

    feat: 添加在线吉他谱

--- src/sections/Project/constants.tsx
+++ src/sections/Project/constants.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { Image } from "antd";
 import {ItemProps} from "./Item";
+import HighLight from "../../components/HighLight";
+
 // Overwatch UI
-import overwatch from '../../assets/icons/overwatch.png'
-import overwatchUI from '../../assets/images/overwatch-ui.png'
+import overwatchLogo from '../../assets/icons/overwatchLogo.png'
+import overwatchUI from '../../assets/screenshots/overwatch-ui.png'
+
+// Guitar Editor
+import guitarLogo from '../../assets/icons/guitarLogo.jpg'
+import guitarEditor from '../../assets/screenshots/guitar-editor.png'
 
 // links
 import link from "../../assets/svgs/link.svg";
@@ -11,21 +17,35 @@ import github from "../../assets/svgs/github.svg";
 
 // 技术栈
 import vue from '../../assets/icons/vue.png';
-import hljs from "../../assets/icons/hljs.png";
+import react from '../../assets/icons/react.png';
 import js from "../../assets/icons/js.png";
 
 const projects: ItemProps[] = [
   {
-    logo: overwatch,
+    logo: overwatchLogo,
     title: 'Overwatch UI',
     content: <Image src={overwatchUI}/>,
-    description: (<p>一个基于守望先锋样式的 UI 库，通过 Vue.js 开发。</p>),
+    description: <p>一个基于 <HighLight>守望先锋</HighLight> 样式的 UI 库，通过 Vue.js 开发。</p>,
     links: [
       {content: 'https://yanhaixiang.com/overwatch-ui-doc/#/', image: link},
       {content: 'https://github.com/Haixiang6123/overwatch-ui', image: github},
     ],
     techUsed: [
       {content: 'Vue.js', image: vue},
+      {content: 'JavaScript', image: js},
+    ]
+  },
+  {
+    logo: guitarLogo,
+    title: '吉他谱编辑器',
+    content: <Image src={guitarEditor}/>,
+    description: (<p>使用 React.js 开发的吉他谱编辑器，在线编辑和生成吉他谱。</p>),
+    links: [
+      {content: 'https://yanhaixiang.com/guitar-editor/#/', image: link},
+      {content: 'https://github.com/Haixiang6123/guitar-editor', image: github},
+    ],
+    techUsed: [
+      {content: 'React.js', image: react},
       {content: 'JavaScript', image: js},
     ]
   }
