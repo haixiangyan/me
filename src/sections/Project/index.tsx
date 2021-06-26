6ab4ef72f4090ca8a8fb2c02b0c415b48562ad98
haixiangyan <haixiang6123@gmail.com>
2021-06-26 13:45:03 +0800

    feat: 使用瀑布流

--- src/sections/Project/index.tsx
+++ src/sections/Project/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Masonry from 'react-masonry-css'
 import styles from './styles.module.scss'
 import Title from "../../components/Title"
 import Section from "../../components/Section"
@@ -11,9 +12,13 @@ const Project = () => {
       <Title tag="h2">个人项目</Title>
       <Title tag="h3">超多好玩、沙雕的项目</Title>
 
-      <ul className={styles.projectList}>
+      <Masonry
+        breakpointCols={3}
+        className={styles.projectList}
+        columnClassName={styles.projectListColumn}
+      >
         {projects.map(project => (<Item {...project}/>))}
-      </ul>
+      </Masonry>
 
       <Title className={styles.more} tag="h3">More projects (and a blog) Coming Soon™ 🕑</Title>
     </Section>
