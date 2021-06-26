4ffdb1827b52600998e0697e63425e0a9a62be67
haixiangyan <haixiang6123@gmail.com>
2021-06-27 01:29:22 +0800

    feat: 使用中文

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {ConfigProvider} from "antd";
 import 'antd/dist/antd.css'
+import zhCN from 'antd/lib/locale/zh_CN';
 import './styles.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>
+), document.getElementById('root'));
