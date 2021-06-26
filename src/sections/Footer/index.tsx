c22183cd992d4595598d9ce0035dcfbadc368445
haixiangyan <haixiang6123@gmail.com>
2021-06-26 18:23:12 +0800

    feat: 完成 Footer 组件

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -1,10 +1,48 @@
+import {Tooltip} from "antd";
 import Paragraph from "../../components/Paragraph";
 import styles from './styles.module.scss'
+
+// 图标
+import react from '../../assets/icons/react.png'
+import antd from '../../assets/icons/antd.png'
+import eslint from '../../assets/icons/eslint.png'
+import ts from '../../assets/icons/ts.png'
+import sass from '../../assets/icons/sass.png'
+
+const techs = [
+  {
+    content: 'React.js',
+    image: react
+  },
+  {
+    content: 'TypeScript',
+    image: ts
+  },
+  {
+    content: 'Eslint',
+    image: eslint
+  },
+  {
+    content: 'Antd',
+    image: antd
+  },
+  {
+    content: 'Sass',
+    image: sass
+  },
+]
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Paragraph>Built with my own 🙌, utilizing:</Paragraph>
+      <Paragraph>本人亲自手写 🙌， 使用了:</Paragraph>
+      <div className={styles.techList}>
+        {techs.map(tech => (
+          <Tooltip placement="top" title={tech.content} color="black">
+            <img src={tech.image} alt="tech"/>
+          </Tooltip>
+        ))}
+      </div>
     </footer>
   )
 }
