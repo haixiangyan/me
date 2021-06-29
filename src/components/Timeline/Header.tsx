9aeed50a93b547bb263478323adbcc23863a0d6d
haixiangyan <haixiang6123@gmail.com>
2021-06-29 23:41:03 +0800

    feat: 添加时间轴的下拉动画

--- src/components/Timeline/Header.tsx
+++ src/components/Timeline/Header.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
+import Fade from 'react-reveal/Fade';
 import styles from './styles.module.scss';
 
 const TimelineHeader: FC = (props) => {
   const { children } = props;
 
   return (
-    <header className={styles.timelineHeader}>
-      <span>{children}</span>
-    </header>
+    <Fade top>
+      <header className={styles.timelineHeader}>
+        <span>{children}</span>
+      </header>
+    </Fade>
   );
 };
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,4 @@ declare module 'react-reveal/Zoom';
 declare module 'react-reveal/Fade';
 declare module 'react-reveal/RubberBand';
 declare module 'react-reveal/Flip';
+declare module 'react-reveal/Slide';
