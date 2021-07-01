fa7643130c9243f4b7730ae495626040dc144103
haixiangyan <haixiang6123@gmail.com>
2021-07-01 14:56:59 +0800

    feat: 添加滚动 active 效果

---
+++ src/hooks/useScrollActive.ts
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+const useScrollActive = (selectors: string[], setActiveItem: Function) => {
+  useEffect(() => {
+    const reversedSelectors = selectors.reverse();
+
+    const onWheel = () => {
+      const $visibleSelector = reversedSelectors.find((selector) => {
+        const $el = document.querySelector(selector);
+
+        if (!$el) return false;
+
+        const { top } = $el.getBoundingClientRect();
+
+        return top < 0;
+      });
+
+      if ($visibleSelector) {
+        setActiveItem($visibleSelector);
+      }
+    };
+
+    window.addEventListener('wheel', onWheel);
+
+    return () => window.removeEventListener('wheel', onWheel);
+  }, []);
+};
+
+export default useScrollActive;
