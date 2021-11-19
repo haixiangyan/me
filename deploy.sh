62b114c391f7616aef0079ed01c7561e58ee7c06
haixiang6123 <haixiang6123@gmail.com>
2021-11-19 13:53:34 +0800

    feat: 更新部署 URL

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Haixiang6123/Haixiang6123.github.io.git master
+git push -f git@github.com:haixiangyan/haixiangyan.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
